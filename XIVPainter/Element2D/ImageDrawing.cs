ccfbf11b6ed4025abd84057e0bf723a88e1542e6
秋水 <1123993881@qq.com>
2023-06-09 11:00:54 +0800

    feat: add image.

---
+++ XIVPainter/Element2D/ImageDrawing.cs
@@ -0,0 +1,18 @@
+﻿namespace XIVPainter.Element2D;
+
+internal class ImageDrawing : IDrawing2D
+{
+    nint _textureId;
+    Vector2 _pt1, _pt2;
+
+    public ImageDrawing(nint textureId, Vector2 pt1, Vector2 pt2)
+    {
+        _textureId = textureId;
+        _pt1 = pt1;
+        _pt2 = pt2;
+    }
+    public void Draw()
+    {
+        ImGui.GetWindowDrawList().AddImage(_textureId, _pt1, _pt2);
+    }
+}
---
+++ XIVPainter/Element3D/Drawing3DImage.cs
@@ -0,0 +1,38 @@
+﻿using ImGuiScene;
+using XIVPainter.Element2D;
+namespace XIVPainter.Element3D;
+
+public class Drawing3DImage : Drawing3D
+{
+    public Vector3 Position { get; set; }
+    public nint ImageID { get; set; }
+    public float Width { get; set; }
+    public float Height { get; set; }
+    public bool HideIfInvisiable { get; set; }
+
+    public Drawing3DImage(TextureWrap wrap, Vector3 position)
+       : this(wrap.ImGuiHandle, position, wrap.Width, wrap.Height)
+    {
+        
+    }
+
+    public Drawing3DImage(nint imageId, Vector3 position, float width, float height)
+    {
+        ImageID = imageId;
+        Position = position;
+        Width = width;
+        Height = height;
+    }
+
+    public override IEnumerable<IDrawing2D> To2D(XIVPainter owner)
+    {
+        if (HideIfInvisiable && !Position.CanSee() || ImageID == 0) return Array.Empty<IDrawing2D>();
+
+        var pts = owner.GetPtsOnScreen(new Vector3[] { Position }, false);
+        if (pts.Length == 0) return Array.Empty<IDrawing2D>();
+        var pt = pts[0];
+
+        var half = new Vector2(Width / 2, Height / 2);
+        return new IDrawing2D[] { new ImageDrawing(ImageID, pt - half, pt + half) };
+    }
+}
